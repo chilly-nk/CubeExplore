c2935e670344da70edcd1f7ba0e6c97556fed418
chilly-nk <narek.ch@gmail.com>
2024-06-17 18:16:33 +0400

    nothing special

--- cubexplore.py
+++ cubexplore.py
@@ -8,6 +8,8 @@ import matplotlib.pyplot as plt
 
 # Specify Data for Testing
 
+import inspect
+
 # project = none
 # experiment = None
 # sample = None
@@ -16,48 +18,48 @@ import matplotlib.pyplot as plt
 # correction_data_path = None
 
 def specify_data(which = str):
-
-#   global project
-#   global experiment
-#   global sample
-#   global data_path
-#   global metadata_path
-#   global correction_data_path
+  caller_globals = inspect.stack()[1][0].f_globals
 
   if which == 'real':
-    globals()['project'] = '/content/drive/My Drive/DATA'
-    experiment = 'Exp 2024-05-03 - 4D HSI Rat Tendon + Nerve'
-    # experiment = 'Test Data' # the same with less cubes
-    sample = 'right_leg_tendon_nerve'
-    dataset = 'cubes'
-
-    data_path = os.path.join(project, experiment, sample, dataset)
-    metadata_path = os.path.join(project, experiment, sample, 'metadata.csv')
-    correction_data_path = os.path.join(project, experiment, sample, 'correction_data.lnk')
+    caller_globals['project'] = '/content/drive/My Drive/DATA'
+    caller_globals['experiment'] = 'Exp 2024-05-03 - 4D HSI Rat Tendon + Nerve'
+    caller_globals['sample'] = 'right_leg_tendon_nerve'
+    caller_globals['dataset'] = 'cubes'
+    
+    caller_globals['data_path'] = os.path.join(project, experiment, sample, dataset)
+    caller_globals['metadata_path'] = os.path.join(project, experiment, sample, 'metadata.csv')
+    caller_globals['correction_data_path'] = os.path.join(project, experiment, sample, 'correction_data.lnk')
 
   elif which == 'test':
-    project = '/content/drive/My Drive/DATA'
-    # experiment = 'Exp 2024-05-03 - 4D HSI Rat Tendon + Nerve'
-    experiment = 'Test Data' # the same with less cubes
-    sample = 'right_leg_tendon_nerve'
-    dataset = 'cubes'
-
-    data_path = os.path.join(project, experiment, sample, dataset)
-    metadata_path = os.path.join(project, experiment, sample, 'metadata.csv')
-    correction_data_path = os.path.join(project, experiment, sample, 'correction_data')
+    caller_globals['project'] = '/content/drive/My Drive/DATA'
+    caller_globals['experiment'] = 'Test Data' # the same with less cubes
+    caller_globals['sample'] = 'right_leg_tendon_nerve'
+    caller_globals['dataset'] = 'cubes'
+    
+    caller_globals['data_path'] = os.path.join(project, experiment, sample, dataset)
+    caller_globals['metadata_path'] = os.path.join(project, experiment, sample, 'metadata.csv')
+    caller_globals['correction_data_path'] = os.path.join(project, experiment, sample, 'correction_data')
 
   print('Data:', data_path)
   print('Metadata:', metadata_path)
   print('Correction Data:', correction_data_path)
 
 
-def test():
-  globals()['testvariable'] = 'nothing'
-  print(testvariable)
+# def test():
+#   globals()['testvariable'] = 'nothing'
+#   print(testvariable)
+
+# mymodule.py
+
+# def define_globals():
+#     global var1, var2
+#     var1 = "Hello"
+#     var2 = "World"
+
 
 # mymodule.py
 
 def define_globals():
-    global var1, var2
-    var1 = "Hello"
-    var2 = "World"
+    caller_globals = inspect.stack()[1][0].f_globals
+    caller_globals['var1'] = "Hello"
+    caller_globals['var2'] = "World"
