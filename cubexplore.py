9bc6b88e90585e87e13f6c9760b6ca69196c4f03
chilly-nk <narek.ch@gmail.com>
2024-06-26 12:29:19 +0400

    Turned get_roi() into a Class

--- cubexplore.py
+++ cubexplore.py
@@ -123,96 +123,81 @@ def process_cubes(cubes, cubes_to_analyse, correction_data_path = None, alter_ba
 
 
 """GET ROIs"""
-# Update 2024-06-26
+# Version 2024-06-26 (2)
 
 """
-Specifying an argument for providing cubes, otherwise when imported does not work
+Turned the function into a class, for importing and reusing convenience
 """
 
-def get_roi(region, cubes, cubes_to_analyse, y1, y2, x1, x2):
+class Regions():
 
-  """
-  Parameters
-  ----------
-  region : str
-    Name of the region
-  cubes_to_analyse : list
-    List of cubes to analyse
-  y1, y2, x1, x2 : int
-    Coordinates of the region, top-to-bottom, left-to-right wise, y2 and x2 are inclusive
-  """
-  global regions
+  def __init__(self):
+    self.regions = {}
 
-  try:
-    regions
-  except NameError:
-    regions = {}
+  def add_roi(self, region, cubes, cubes_to_analyse, y1, y2, x1, x2):
+    
+    self.regions[region] = {}
+    
+    rows = slice(y1, y2+1)
+    cols = slice(x1, x2+1)
 
-  # Store Coordinates
-  regions[region] = {}
+    self.regions[region]['rows'] = rows
+    self.regions[region]['cols'] = cols
+    self.regions[region]['rows_str'] = f'{rows.start}:{rows.stop}'
+    self.regions[region]['cols_str'] = f'{cols.start}:{cols.stop}'
+    self.regions[region]['num_pixels'] = (rows.stop - rows.start) * (cols.stop - cols.start)
 
-  rows = slice(y1, y2+1)
-  cols = slice(x1, x2+1)
+    # Delete
+    # rows = self.regions[region]['rows']
+    # cols = self.regions[region]['cols']
 
-  regions[region]['rows'] = rows
-  regions[region]['cols'] = cols
-  regions[region]['rows_str'] = f'{rows.start}:{rows.stop}'
-  regions[region]['cols_str'] = f'{cols.start}:{cols.stop}'
-  regions[region]['num_pixels'] = (rows.stop - rows.start) * (cols.stop - cols.start)
+    spectra_corrected = {}
+    spectra_corrected['byCube'] = {}
+    spectra_corrected['Combined'] = {}
 
-  # Delete
-  # rows = regions[region]['rows']
-  # cols = regions[region]['cols']
+    eems_corrected = {}
 
-  spectra_corrected = {}
-  spectra_corrected['byCube'] = {}
-  spectra_corrected['Combined'] = {}
+    ###
+    spectra = {}
+    spectrum = {}
 
-  eems_corrected = {}
+    for cube in cubes_to_analyse:
 
-  ###
-  spectra = {}
-  spectrum = {}
+      # Get wavelengths from cubes metadata
+      # This can be transferred to the cubes metadata, and later just accessed like cubes[cube]['wavelengths']
+      start = cubes[cube]['em_start']
+      end = cubes[cube]['em_end']
+      step = cubes[cube]['step']
+      wavelengths = range(start, end+1, step)
 
-  for cube in cubes_to_analyse:
+      # Slice each cube to the region's coordinates
+      cube_segment = cubes[cube]['data_corrected'][rows, cols, :]
 
-    # Get wavelengths from cubes metadata
-    # This can be transferred to the cubes metadata, and later just accessed like cubes[cube]['wavelengths']
-    start = cubes[cube]['em_start']
-    end = cubes[cube]['em_end']
-    step = cubes[cube]['step']
-    wavelengths = range(start, end+1, step)
+      # Reshape each cube and get 2D data for spectra for each pixel
+      cube_reshaped = cube_segment.reshape(cube_segment.shape[0] * cube_segment.shape[1], cube_segment.shape[2])
+      spectra[cube] = pd.DataFrame(cube_reshaped, columns = wavelengths)
 
-    # Slice each cube to the region's coordinates
-    cube_segment = cubes[cube]['data_corrected'][rows, cols, :]
+      # Get the average spectra by pixels for each cube
+      cube_averaged_byPixel = np.mean(cube_segment, axis = (0, 1), keepdims = True).reshape(1, cube_segment.shape[2])
+      spectrum[cube] = pd.DataFrame(cube_averaged_byPixel, columns = wavelengths, index = [int(cubes[cube]['ex'])])
 
-    # Reshape each cube and get 2D data for spectra for each pixel
-    cube_reshaped = cube_segment.reshape(cube_segment.shape[0] * cube_segment.shape[1], cube_segment.shape[2])
-    spectra[cube] = pd.DataFrame(cube_reshaped, columns = wavelengths)
+      # Get EEM out of averaged (over pixels) spectra, by stacking them together
+      try:
+        eem_averaged_corrected
+      except NameError:
+        eem_averaged_corrected = pd.DataFrame(columns = wavelengths)
 
-    # Get the average spectra by pixels for each cube
-    cube_averaged_byPixel = np.mean(cube_segment, axis = (0, 1), keepdims = True).reshape(1, cube_segment.shape[2])
-    spectrum[cube] = pd.DataFrame(cube_averaged_byPixel, columns = wavelengths, index = [int(cubes[cube]['ex'])])
+      eem_averaged_corrected = pd.concat([eem_averaged_corrected, spectrum[cube]], axis = 0).sort_index(ascending = False)
 
-    # Get EEM out of averaged (over pixels) spectra, by stacking them together
-    try:
-      eem_averaged_corrected
-    except NameError:
-      eem_averaged_corrected = pd.DataFrame(columns = wavelengths)
+    spectra_corrected['byCube']['all'] = spectra
+    spectra_corrected['byCube']['average'] = spectrum
 
-    eem_averaged_corrected = pd.concat([eem_averaged_corrected, spectrum[cube]], axis = 0).sort_index(ascending = False)
+    self.regions[region]['eem_averaged_corrected'] = eem_averaged_corrected
 
-  spectra_corrected['byCube']['all'] = spectra
-  spectra_corrected['byCube']['average'] = spectrum
+    spectra_corrected['Combined']['all'] = None
+    spectra_corrected['Combined']['average'] = None
 
-  regions[region]['eem_averaged_corrected'] = eem_averaged_corrected
-
-  spectra_corrected['Combined']['all'] = None
-  spectra_corrected['Combined']['average'] = None
-
-  regions[region]['spectra_corrected'] = spectra_corrected
-
-  return regions
+    self.regions[region]['spectra_corrected'] = spectra_corrected 
 
 
 
