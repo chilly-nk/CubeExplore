8c796f68c0dfc970629030e7706bfed16ddd563c
chilly-nk <narek.ch@gmail.com>
2025-12-06 16:32:22 +0000

    New Feature | spectra.FWHM | Calculate full width half maximum for a spectrum (curve) given by x(wavlength) and y(intensity).

--- cubexplore/spectra.py
+++ cubexplore/spectra.py
@@ -35,3 +35,47 @@ def read_maya(path, skiprows=13):
   data['average_intensity'] = data.mean(axis=1)
   return data
 
+class FWHM():
+  def __init__(self, x: np.ndarray, y: np.ndarray):
+    """
+    Parameters:
+    x: 1-D array of wavelengths
+    y: 1-D array of intensities. Must correspond to a spectrum with exactly one peak.
+    """
+
+    peak_idx = np.argmax(y)
+    peak_x = x[peak_idx]
+    peak_y = y[peak_idx]
+    half_max = peak_y / 2
+
+    left_idx = np.where(y[:peak_idx] < half_max)[0]
+    if len(left_idx) > 0:
+      i1 = left_idx[-1]
+      i2 = i1 + 1
+    x_left = np.interp(half_max, [y[i1], y[i2]], [x[i1], x[i2]])
+
+    right_idx = np.where(y[peak_idx:] < half_max)[0]
+    if len(right_idx) > 0:
+      i2 = peak_idx + right_idx[0]
+      i1 = i2 - 1
+    x_right = np.interp(half_max, [y[i1], y[i2]], [x[i1], x[i2]])
+
+    fwhm = x_right - x_left
+
+    fwhm_center = x_left + fwhm / 2
+    peak_offset = fwhm_center - peak_x
+
+    self.x = x
+    self.y = y
+    self.peak_idx = peak_idx
+    self.peak_x = peak_x
+    self.peak_y = peak_y
+    self.half_max = half_max
+    self.left_idx = left_idx
+    self.x_left = x_left
+    self.right_idx = right_idx
+    self.x_right = x_right
+    self.fwhm = fwhm
+    self.fwhm_center = fwhm_center
+    self.peak_offset = peak_offset
+
